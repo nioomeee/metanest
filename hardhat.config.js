0e3a0eb73fa901b33d50f59986df7a3e8f02c898
Niomi <nioomeee@gmail.com>
2025-06-10 22:38:46 +0530

    part of setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config(); // ⬅️ To use .env variables
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
   networks: {
     hardhat: {
       chainId: 31337,
     },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    polygon: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY, // Optional: For contract verification
   },
 };
 
