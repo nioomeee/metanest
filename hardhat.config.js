c154779995d93b58e217092ec1a8de1e89266665
Niomi <nioomeee@gmail.com>
2025-07-03 19:01:40 +0530

    changed the versions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
-require('@nomicfoundation/hardhat-toolbox');
-require('@openzeppelin/hardhat-upgrades');
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
 
 module.exports = {
   solidity: "0.8.20",
